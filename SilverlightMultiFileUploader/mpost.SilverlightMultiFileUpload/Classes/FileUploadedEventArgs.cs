3d6e70ab89df74ac5f8ada5cd57f187fa81d34e0
mpost_cp <SND\mpost_cp>
2010-02-06 17:48:09 +0000

    Created FileUploadedEventArgs with filename and size data (SL4 VS2010 ONLY)

---
+++ SilverlightMultiFileUploader/mpost.SilverlightMultiFileUpload/Classes/FileUploadedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Browser;
+
+namespace mpost.SilverlightMultiFileUpload.Classes
+{
+    [ScriptableType]
+    public class FileUploadedEventArgs : EventArgs
+    {
+        [ScriptableMember()]
+        public string FileName { get; set; }
+        public double FileSize { get; set; }
+
+        public FileUploadedEventArgs(string fileName, double fileSize)
+        {
+            FileName = fileName;
+            FileSize = fileSize;
+        }
+    }
+}
