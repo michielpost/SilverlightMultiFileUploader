6048a5ebbac006d610962aea6bea32759c3cac9e
Michiel Post <michielpost@users.noreply.github.com>
2010-04-13 17:28:48 +0000

    Silverlight 4 RC All usermessages put into 1 resource file for easy localization

---
+++ SilverlightMultiFileUploader/mpost.SilverlightMultiFileUpload/Classes/UserMessages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace mpost.SilverlightMultiFileUpload.Classes
+{
+    public class UserMessages
+    {
+        public UserMessages()
+        {
+        }
+
+        private static mpost.SilverlightMultiFileUpload.UserMessages resource = new SilverlightMultiFileUpload.UserMessages();
+
+        public mpost.SilverlightMultiFileUpload.UserMessages UserMessagesResource { get { return resource; } }
+    }
+
+}
