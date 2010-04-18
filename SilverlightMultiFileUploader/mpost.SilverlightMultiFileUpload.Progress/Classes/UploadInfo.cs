3c635eca9a26f800b6155750a18538e775e3a131
Michiel Post <michielpost@users.noreply.github.com>
2010-04-18 11:22:32 +0000

    Added optional progress bar control for the LITE version (see LITE test pages)

---
+++ SilverlightMultiFileUploader/mpost.SilverlightMultiFileUpload.Progress/Classes/UploadInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace mpost.SilverlightMultiFileUpload.Progress.Classes
+{
+    public class UploadInfo : INotifyPropertyChanged
+    {
+        private float _percentage = 0;
+
+        public float Percentage
+        {
+            get { return _percentage; }
+            set
+            {
+                _percentage = value;
+                NotifyPropertyChanged("Percentage");
+
+
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        private void NotifyPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
